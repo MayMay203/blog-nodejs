c9102de08498f4b08146af6a098f22e16009b27a
Your Name <you@example.com>
2024-02-19 23:17:53 +0700

    add morgan

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const express = require('express')
+const morgan = require('morgan')
 const app = express()
+ 
+app.use(morgan('combined'))
 const port = 3000
 
 app.get('/tin-tuc', (req, res)=> {
-  var a = 1
-  var b = 2
-  var c = a + b
+  
   return res.send('Hello World!')
 })
 
