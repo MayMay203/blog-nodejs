678315c1ef1f16d85454bc3eb69da862e3bbd22d
Your Name <you@example.com>
2024-02-26 22:10:24 +0700

    refactor code: create routes, app/controller folders

--- src/index.js
+++ src/index.js
@@ -5,9 +5,12 @@ const handlebars = require("express-handlebars").engine
 // import { engine } from 'express-handlebars';
 const app = express()
 app.use(express.static(path.join(__dirname+'/public')))
-const port = 3009
-// HTTP logger
 app.use(morgan('combined'))
+app.use(express.urlencoded({
+  extended: true
+}))
+app.use(express.json())
+// XMLHTTPRequest,fetch, axios, ajax
 // Template engine
 app.engine('hbs', handlebars({
   extname: '.hbs'
@@ -15,15 +18,13 @@ app.engine('hbs', handlebars({
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'resources/views'));
 // console.log("Path: ",path.join(__dirname, 'resources/views'))
+const port = 3009
 
-app.get('/', (req, res)=> {
-   res.render('home')
-})
-
-app.get('/news', (req, res)=> {
-  res.render('news')
-})
-
+//Import file index.js
+const route = require('./routes')
+// Routes inits
+route(app)
+// Action ---> Dispatcher ---> Function handler
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
---
+++ src/routes/index.js
@@ -0,0 +1,27 @@
+// Tat ca file trong route phai nap vao index.js
+const newRoute = require('./news')
+const siteRoute = require('./site')
+
+function route(app){
+    // app.get('/', (req, res)=> {
+    //     res.render('home')
+    //  })
+     
+    //  app.get('/news', (req, res)=> {
+    //    res.render('news')
+    //  })
+
+    // app.get("/search",(req,res)=>{
+    //     // console.log(req.query.q)
+    //     res.render("search")
+    //   })
+    app.use('/news',newRoute)
+    app.use('/',siteRoute)
+     
+    //  app.post("/search",(req,res)=>{
+    //    console.log(req.body)
+    //    res.render("search")  
+    //  })
+}
+
+module.exports = route
