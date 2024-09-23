c219bc545e2aa3176090ba5e5e339fe7e7c34d5f
Nhung <hongnhung16052003@gmail.com>
2024-09-23 16:55:59 +0700

    refactor code: create routes, app/controllers folder

---
+++ src/app/controllers/SiteController.js
@@ -0,0 +1,15 @@
+class SiteController{
+    index(req, res) {
+        res.render('home')
+    }
+
+    search(req, res) {
+        res.send('Search')
+    }
+
+    contact(req, res) {
+        res.send('Contact')
+    }
+}
+
+module.exports = new SiteController
---
+++ src/index.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const morgan = require('morgan')
+const handlebars = require('express-handlebars').engine;
+const path = require('path')
+const app = express()
+const port = 3000
+
+// HTTP logger
+app.use(morgan('combined'))
+
+// Template engine
+app.engine('hbs', handlebars({ extname: '.hbs' }));
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'resources/views'));
+
+// Config static file
+app.use(express.static(path.join(__dirname, 'public')))
+
+const route = require('./routes')
+
+// Routes init
+route(app)
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
