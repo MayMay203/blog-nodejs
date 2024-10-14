46298feeb706830905d4d91d52ba09dedac2757f
Nhung <hongnhung16052003@gmail.com>
2024-10-14 17:48:31 +0700

    handle id autoincrement with mongoose sequence

--- src/models/Course.js
+++ src/models/Course.js
@@ -2,9 +2,11 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 const slug = require('mongoose-slug-updater')
 const mongooseDelete = require('mongoose-delete')
+const AutoIncrement = require('mongoose-sequence')(mongoose)
 
 const Course = new Schema(
   {
+    _id: { type: Number },
     name: { type: String, required: true },
     description: { type: String },
     image: { type: String },
@@ -13,12 +15,14 @@ const Course = new Schema(
     slug: { type: String, slug: 'name', unique: true },
   },
   {
+    _id: false,
     timestamps: true,
   },
 )
 
 // Add plugin
 mongoose.plugin(slug)
+Course.plugin(AutoIncrement);//Mặc định là áp dụng cho trường id
 Course.plugin(mongooseDelete, { overrideMethods: 'all', deletedAt: true })
 
 module.exports = mongoose.model('Course', Course)
